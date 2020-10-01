2fec477499f418f8c77f7afac62c4912798fc96a
Leonardo Veiga <leonardo.porfirio@ccc.ufcg.edu.br>
2020-10-01 17:34:52 -0300

    variáveis globais

---
+++ node/global/global.js
@@ -0,0 +1,8 @@
+// console.log(global)
+
+global.MeuApp = {
+    saudacao() {
+        return 'saudacao meu parcero'
+    },
+    nome: 'MeuApp'
+}
---
+++ node/global/globalCliente.js
@@ -0,0 +1,6 @@
+require('./global')
+
+console.log(MeuApp.saudacao())
+
+MeuApp.nome = 'Mudei de nome kkkkk'
+console.log(MeuApp.nome)
