131df5c8c8f981cb329ebefa83cddd550f74e89b
Leonardo Veiga <leonardo.porfirio@ccc.ufcg.edu.br>
2020-10-01 18:09:13 -0300

    leitura de JSONs (async e sync)

--- node/leituraArquivos/arquivoLeitura.js
+++ node/leituraArquivos/arquivoLeitura.js
@@ -13,12 +13,13 @@ fs.readFile(caminho, 'utf-8', (err, Arquivojson) => {
     console.log(`isso aqui executou depois: ${config.db.host}:${config.db.port}`)
 })
 
+// leu o conteúdo do diretório atual pelo __dirname (retorna o caminho ate o diretorio atual)
+// de forma assíncrona
+fs.readdir(__dirname, (err, arquivos) => {
+    console.log('Conteúdo da pasta assíncrona: ')
+    console.log(arquivos)
+})
+
 // simplificando a leitura de um json de forma síncrona
 const config = require('./arquivo.json') // importa ja como objeto
 console.log(`isso aqui executou antes: ${config.db.host}:${config.db.port}`)
-
-// __dirname é o caminho até aqui
-fs.readdir(__dirname, (err, arquivos) => {
-    console.log('Conteúdo da pasta: ')
-    console.log(arquivos)
-})
