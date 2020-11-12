fe045a879ded9aa2f2f71fc148c4d3864b6fb828
Leonardo Veiga <leonardo.porfirio@ccc.ufcg.edu.br>
2020-11-12 04:01:30 -0300

    criando contador de nota de loac

--- node/arquivosLeitura/arquivoLeitura.js
+++ node/arquivosLeitura/arquivoLeitura.js
@@ -1,25 +1,24 @@
-const fs = require('fs')
+const fs = require("fs");
 
-
-const caminho = __dirname + '/arquivo.json'
+const caminho = __dirname + "/arquivo.json";
 
 // síncrono
-const conteudo = fs.readFileSync(caminho, 'utf-8')
-console.log(conteudo)
+const conteudo = fs.readFileSync(caminho, "utf-8");
+console.log(conteudo);
 
 // assíncrono
-fs.readFile(caminho, 'utf-8', (err, Arquivojson) => {
-    const config = JSON.parse(Arquivojson)
-    console.log(`isso aqui executou depois: ${config.db.host}:${config.db.port}`)
-})
+fs.readFile(caminho, "utf-8", (err, Arquivojson) => {
+	const config = JSON.parse(Arquivojson);
+	console.log(`isso aqui executou depois: ${config.db.host}:${config.db.port}`);
+});
 
 // leu o conteúdo do diretório atual pelo __dirname (retorna o caminho ate o diretorio atual)
 // de forma assíncrona
 fs.readdir(__dirname, (err, arquivos) => {
-    console.log('Conteúdo da pasta assíncrona: ')
-    console.log(arquivos)
-})
+	console.log("Conteúdo da pasta assíncrona: ");
+	console.log(arquivos);
+});
 
 // simplificando a leitura de um json de forma síncrona
-const config = require('./arquivo.json') // importa ja como objeto
-console.log(`isso aqui executou antes: ${config.db.host}:${config.db.port}`)
+const config = require("./arquivo.json"); // importa ja como objeto
+console.log(`isso aqui executou antes: ${config.db.host}:${config.db.port}`);
---
+++ node/requisicao/contarnotaLOAC.js
@@ -0,0 +1,30 @@
+const url = "http://lad.ufcg.edu.br/loac/uploads/OAC/anon.txt";
+const caminho = __dirname + "/notas.txt";
+const axios = require("axios");
+const fs = require("fs");
+
+filtraNotas = (notas, idAlvo) => {
+	let listaSplit = notas.split("\n");
+	let mapaNotas = new Map();
+	let nota = listaSplit[0].split(" ");
+	let i = 0;
+	while (nota[0] != idAlvo) {
+		// console.log(nota[0]);
+		i++;
+		nota = listaSplit[i].split(" ");
+	}
+	let notaAluno = 0;
+	while (idAlvo == listaSplit[i].split(" ")[0]) {
+		notaAluno += parseInt(listaSplit[i].split(" ")[2]);
+		i++;
+	}
+	return notaAluno;
+};
+
+axios.get(url).then((response) => {
+	const posts = response.data;
+	const idAluno = fs.readFileSync(caminho, "utf8");
+	console.log(idAluno);
+	let notaFiltrada = "Centavos: " + filtraNotas(posts, idAluno);
+	fs.writeFileSync(caminho, notaFiltrada);
+});
